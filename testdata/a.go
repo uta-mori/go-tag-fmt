19c59c6bf655eb28af8bc14b33f4317dbd48cc19
uta-mori <mori_yuta@retail-ai.jp>
2020-12-15 15:23:19 +0900

    ♻️  test update

--- testdata/a.go
+++ testdata/a.go
@@ -1,6 +1,6 @@
 package a
 
 type Resource struct {
-        ID   int64    `json:"id" xml:"id"`
-        Data []string `json:"data,omitempty" xml:"data"`
+        ID   int64    `json:"id"    xml:"id"`
+        Data []string `json:"data,omitempty"           xml:"data"`
 }
